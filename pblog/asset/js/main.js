66d05207ef10dbc074f002247555200750bfe9ca
picospuch <picospuch@gmail.com>
2016-07-09 22:30:09 +0800

    update blog to pblog

---
+++ pblog/asset/js/main.js
@@ -0,0 +1,12 @@
+$(function ready() {
+function setupDuoshuo() {
+  $('#myduoshuo .ds-thread').attr('data-url', window.location.origin + window.location.pathname);
+}
+
+setupDuoshuo();
+
+
+
+
+
+}); //~ready
